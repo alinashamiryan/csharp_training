91e3ddbf0230c42a115167d1d74f9041e1c271f7
Шамирян Алина Александровна <A.Shamiryan@norbit.ru>
2021-10-16 22:50:41 +0300

    Создан тест контактов ContactCreationTests

---
+++ addressbook-web-tests/ContactDate.cs
@@ -0,0 +1,43 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace WebAddresbookTests
+{
+    class ContactDate
+    {
+        private string firstname;
+        public string Firstname
+        {
+            get
+            {
+                return firstname;
+            }
+            set
+            {
+                firstname = value;
+            }
+        }
+
+        private string lastname;
+        public string Lastname
+        {
+            get
+            {
+                return lastname;
+            }
+            set
+            {
+                lastname = value;
+            }
+        }
+        public ContactDate (string firstname, string lastname)
+        {
+            this.firstname = firstname;
+            this.lastname = lastname;
+        }
+
+    }
+}
